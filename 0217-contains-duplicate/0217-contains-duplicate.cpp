146a624f82482f1f40c9ee814c6c70154847f892
Amleshwar Pandey <76100810+amleshwar-p@users.noreply.github.com>
2023-10-05 01:02:19 +0530

    Time: 93 ms (72.29%), Space: 57.9 MB (70.69%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        bool flag = false;
+
+        for(int i =0;i<n-1;i++){
+            
+                if(nums[i]==nums[i+1]){
+                    return true;
+                
+            }
+        }
+        return flag;
+        
+    }
+};
