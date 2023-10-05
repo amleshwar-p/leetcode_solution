977975f43c943ebdb35a1f6dabe93e83b20d843e
Amleshwar Pandey <76100810+amleshwar-p@users.noreply.github.com>
2023-10-05 23:22:43 +0530

    Time: 3 ms (88.84%), Space: 10.5 MB (6.06%) - LeetHub

--- 1168-duplicate-zeros/1168-duplicate-zeros.cpp
+++ 1168-duplicate-zeros/1168-duplicate-zeros.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     void duplicateZeros(vector<int>& arr) {
-        int n = arr.size();
-        vector<int>ans;
-
-        for(int i =0;i<n;i++){
-            ans.push_back(arr[i]);
+        vector<int> v;
+        for(int i=0; i<arr.size(); i++){
+            v.push_back(arr[i]);
             if(arr[i]==0){
-                ans.push_back(0);
+                v.push_back(0);
+                    
+                }
+            }
+            for(int i=0; i<arr.size(); i++){
+            arr[i]=v[i];
             }
         }
-        for(int i=0;i<n;i++){
-            arr[i]=ans[i];
-        }
-    }
+    
+    
 };
