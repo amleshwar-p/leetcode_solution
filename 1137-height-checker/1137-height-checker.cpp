e15ecc84e103358f8c4843b1cc7931829058b6cc
Amleshwar Pandey <76100810+amleshwar-p@users.noreply.github.com>
2023-10-07 15:15:30 +0530

    Time: 4 ms (37.9%), Space: 8.4 MB (24.57%) - LeetHub

--- 1137-height-checker/1137-height-checker.cpp
+++ 1137-height-checker/1137-height-checker.cpp
@@ -3,16 +3,18 @@ public:
     int heightChecker(vector<int>& heights) {
 
         int ans=0;
-        vector<int>expected;
-        int n = heights.size();
-        for(int i =0;i<n;i++){
-            expected.push_back(heights[i]);
+        vector<int>v;
+        int n= heights.size();
+        for(int i=0;i<n;i++){
+            v.push_back(heights[i]);
         }
-        sort(expected.begin(),expected.end());
+
+        sort(v.begin(),v.end());
+
         for(int i =0;i<n;i++){
-        if(heights[i]!=expected[i]){
-            ans++;
-        }
+            if(heights[i]!=v[i]){
+                ans++;
+            }
         }
         return ans;
         
