8052805bfa456fdeafbfebb47dce4ab9b975be50
Amleshwar Pandey <76100810+amleshwar-p@users.noreply.github.com>
2024-06-17 14:02:42 +0530

    Time: 151 ms (28.99%) | Memory: 143.1 MB (7.49%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -6,18 +6,14 @@ public:
         vector<int> v1;
         vector<int> v2;
         vector<int> v3;
-
-
         for (int i = 0; i < n; i++) {
             if(nums[i]>0){
                 v1.push_back(nums[i]);  
 
             }else{
                 v2.push_back(nums[i]);  
-
             }
         }
-
         int v1size =0;
         int v2size =0;
         for(int i =0;i<v1size<v1.size() && v2size< v2.size();i++){
