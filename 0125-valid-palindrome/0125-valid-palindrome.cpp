4dcf846a793f3f1071adfb8244bb7b659779d0ae
Amleshwar Pandey <76100810+amleshwar-p@users.noreply.github.com>
2024-09-11 19:10:00 +0530

    Time: 4 ms (80.02%), Space: 9.3 MB (30.87%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,56 +1,21 @@
 class Solution {
-private:
-    bool validChar(char ch) {
-        if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') ||
-            (ch >= '0' && ch <= '9')){
-                return 1;
-            }else{
-                return 0;
-            }
-    }
-    char toLower(char ch){
-        if((ch>='a'&& ch<='z') || (ch>='0'&& ch<='9') ){
-            return ch;
-        }else{
-            char temp = ch - 'A' + 'a';
-            return temp;
-        }
-    }
-
-    bool palCheck(string a){
-
-        int low=0;
-        int high=a.length()-1;
-
-        while(low<=high){
-            if(a[low]!=a[high]){
-                return 0;
-            }else{
-                low++;
-                high--;
-            }
-        }
-        return 1;
-
-    }
 public:
     bool isPalindrome(string s) {
-        // remove useless characters
-        string tmp ="";
-        for(int j=0;j<s.length();j++){
-            if(validChar(s[j])){
-                tmp.push_back(s[j]);
+        string str;
+        for (int i = 0; i < s.size(); i++) {
+            if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z') ||
+                (s[i] >= '0' && s[i] <= '9')) {
+                if ((s[i] >= 'A' && s[i] <= 'Z')) {
+                    s[i] += 32;
+                }
+                str += s[i];
             }
         }
-
-        // convert to lowercase
-
-        for(int i=0;i<tmp.length();i++){
-            tmp[i] = toLower(tmp[i]);
+        for (int i = 0; i < str.size() / 2; i++) {
+            if (str[i] != str[str.size() - 1 - i]) {
+                return false;
+            }
         }
-
-        // check palindrome
-
-        return palCheck(tmp);
+        return true;
     }
 };
