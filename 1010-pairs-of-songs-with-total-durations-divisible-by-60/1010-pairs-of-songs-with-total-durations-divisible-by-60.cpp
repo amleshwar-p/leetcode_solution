b1cb3f55c65f33833aab669cf7b8dad56ec87c23
Amleshwar Pandey <76100810+amleshwar-p@users.noreply.github.com>
2024-09-14 16:07:36 +0530

    Time: 22 ms (87.71%), Space: 30.7 MB (75.94%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.cpp
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.cpp
@@ -3,19 +3,14 @@ public:
     int numPairsDivisibleBy60(vector<int>& time) {
         int n = time.size();
         vector<int> count(60, 0);
-
-        // for (int i : time) {
-        //     count[i % 60]++;
-        // }
-        for (int i = 0; i < time.size(); i++) {
-            count[time[i] % 60]++;
-        }
         int ans = 0;
-        for (int i = 1; i < 30; i++) {
-            ans += count[i] * count[60 - i];
+        for (int i : time) {
+            int rem_a = i % 60;           // rem of a will be seen % with 60
+            int comp = (60 - rem_a) % 60; // compliement check of rem a
+
+            ans += count[comp];
+            count[rem_a]++;
         }
-        ans += (count[0] * (count[0] - 1) / 2);
-        ans += (count[30] * (count[30] - 1) / 2);
         return ans;
     }
 };
