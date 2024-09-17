91aa7be9275303e4c7d9285021ae89904fe4b63c
Amleshwar Pandey <76100810+amleshwar-p@users.noreply.github.com>
2024-09-17 14:05:51 +0530

    Time: 39 ms (80.75%) | Memory: 51.1 MB (80.73%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+
+        int ans = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > ans) {
+                return false;
+            }
+            ans = max(ans, nums[i] + i);
+        }
+        return true;
+    }
+};
