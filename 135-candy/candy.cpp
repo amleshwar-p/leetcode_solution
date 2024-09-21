7730c891ac245a8d8aed613c9995e722ea83b1b9
Amleshwar Pandey <76100810+amleshwar-p@users.noreply.github.com>
2024-09-21 12:31:03 +0530

    Time: 16 ms (49.38%) | Memory: 20.3 MB (59.54%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
+
         int n = ratings.size();
         int ans = 0;
         vector<int> v(n, 1);
 
-        // left pass
+        // left pass(from start)
         for (int i = 1; i < n; i++) {
             if (ratings[i] > ratings[i - 1]) {
                 v[i] = v[i - 1] + 1;
             }
         }
-        // right pass
+        // right pass(from end)
         for (int i = n - 2; i >= 0; i--) {
             if (ratings[i] > ratings[i + 1]) {
-
-                v[i] = max(v[i],v[i + 1] + 1);
+                v[i] = max(v[i], v[i + 1] + 1);
             }
         }
         for (int i = 0; i < n; i++) {
