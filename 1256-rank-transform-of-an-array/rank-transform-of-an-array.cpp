98316f78efce1f29fac5925a9c095a4871ac0c1a
Amleshwar Pandey <76100810+amleshwar-p@users.noreply.github.com>
2024-10-03 01:05:00 +0530

    Time: 115 ms (13.72%) | Memory: 44 MB (41.66%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
+
         int n = arr.size();
         int rank = 1;
-        vector<int> result;
-
         map<int, int> mp;
         for (auto it : arr) {
             mp[it]++;
         }
-        // rank assign
         for (auto it : mp) {
-            mp[it.first] = rank;
-            rank++;
+            mp[it.first] = rank++;
+            // rank++;
         }
-        // ab push krdo final values
+        vector<int> ans;
         for (int i = 0; i < n; i++) {
-            result.push_back(mp[arr[i]]);
+            ans.push_back(mp[arr[i]]);
         }
-        return result;
-
-        // map  bnaya aur key value store kraya
-        // ab rank assign krenge
+        return ans;
     }
 };
