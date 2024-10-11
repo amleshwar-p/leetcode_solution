e9bafec8d2811b767e800b3f42ea9c7d16cee285
Amleshwar Pandey <76100810+amleshwar-p@users.noreply.github.com>
2024-10-11 21:53:05 +0530

    Time: 11 ms (69.80%) | Memory: 23.1 MB (55.89%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.cpp
+++ 605-can-place-flowers/can-place-flowers.cpp
@@ -5,18 +5,15 @@ public:
         int x = flowerbed.size();
 
         for (int i = 0; i < x; i++) {
-
-            // base case check
+            // base case
             if (n == 0) {
                 return true;
             }
-
             if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) &&
                 (i == x - 1 || flowerbed[i + 1] == 0)) {
-                // put a flower
+                // put a plant
                 flowerbed[i] = 1;
                 n--;
-
                 if (n == 0) {
                     return true;
                 }
