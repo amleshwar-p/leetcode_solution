2130d272bbb5f4e49264a1c59d5f6b6843823ea1
Amleshwar Pandey <76100810+amleshwar-p@users.noreply.github.com>
2024-10-20 19:04:56 +0530

    Time: 7 ms (98.46%) | Memory: 61.7 MB (74.41%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -8,30 +8,17 @@ public:
         int right = n - 1;
 
         while (left < right) {
-            // calculate area of water stored area that is (height* width)
+            // calculate area of water stored ( height x width)
             int area = min(height[left], height[right]) * (right - left);
-            // if height is smaller than move it to next
+            // if height small so move to next
             if (height[left] < height[right]) {
                 left++;
             } else {
                 right--;
             }
-            // store max value
+            // update and store max value
             ans = max(ans, area);
         }
         return ans;
     }
 };
-// Two-pointer technique is the optimal way to solve this problem.
-// You will use two pointers, one starting at the beginning (left)
-//  and the other at the end (right) of the height array.
-
-// You calculate the area formed by the lines at the left and right pointers by
-// the formula: Area=min(height[left],height[right])×(right−left)
-
-// Then, you move the pointer which has the smaller height, either left or
-// right,
-//   inward, because the smaller height is the limiting factor for the
-//   container's water capacity.
-
-// Keep updating the maximum area as you move the pointers.
