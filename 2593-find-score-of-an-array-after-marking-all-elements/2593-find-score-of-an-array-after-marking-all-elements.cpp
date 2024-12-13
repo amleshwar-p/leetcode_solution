1f28182592ff10b4b510e77dd920d345d2ffbbed
Amleshwar Pandey <76100810+amleshwar-p@users.noreply.github.com>
2024-12-14 02:53:13 +0530

    Time: 55 ms (92.81%), Space: 105.6 MB (68.87%) - LeetHub

--- 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.cpp
+++ 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.cpp
@@ -3,11 +3,13 @@ public:
     long long findScore(vector<int>& nums) {
         int n = nums.size();
 
-        vector<pair<int, int>> v1;
-        // store in pair
+        vector < pair<int, int>> v1;
+
         for (int i = 0; i < n; i++) {
             v1.push_back({nums[i], i});
         }
+
+        // now sort
         sort(v1.begin(), v1.end());
 
         long long score = 0;
@@ -15,18 +17,19 @@ public:
         vector<bool> visited(n, false);
 
         for (int i = 0; i < n; i++) {
-            int elmnt = v1[i].first;
-            int indx = v1[i].second;
+            int element = v1[i].first;
+            int index = v1[i].second;
 
-            if (visited[indx] == false) {
-                visited[indx] = true;
-                score += elmnt;
+            if (visited[index] == false) {
+                visited[index] = true;
+                score += element;
 
-                if (indx - 1 >= 0) {
-                    visited[indx - 1] = true;
+                // check adjacent
+                if (index - 1 >= 0) {
+                    visited[index - 1] = true;
                 }
-                if (indx + 1 < n) {
-                    visited[indx + 1] = true;
+                if (index + 1 <= n) {
+                    visited[index + 1] = true;
                 }
             }
         }
