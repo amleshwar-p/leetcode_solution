b13487da0c12b359a875ebea36421e12318c2a17
Amleshwar Pandey <76100810+amleshwar-p@users.noreply.github.com>
2025-02-11 02:06:29 +0530

    Time: 0 ms (100%), Space: 8.5 MB (70.3%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+
+        int i = 0;
+        while (i < s.length()) {
+            if (isdigit(s[i])) {
+                s.erase(i, 1);
+                if (i - 1 >= 0) {
+                    s.erase(i - 1, 1);
+                    i--;
+                }
+            } else {
+                i++;
+            }
+        }
+        return s;
+    }
+};
